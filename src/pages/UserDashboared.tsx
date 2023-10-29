90b4448a8837fdbbc4b0256d6b96111a0bfb1660
Farah <alharbi.farah@hotmail.com>
2023-10-29 23:38:06 +0300

    add MUI for buttons

--- src/pages/UserDashboared.tsx
+++ src/pages/UserDashboared.tsx
@@ -1,14 +1,19 @@
 import UserSidebar from '../components/UserSidebar'
+import { ThemeProvider } from '@mui/material/styles';
+
+import themes from '../Theme/Themes';
 
 const UserDashboared = () => {
   return (
-    <div className='container'>
-      <UserSidebar />
-      <div className='main-content'>
-        <h2>User profile gose here</h2>
-        <p>main content goes here</p>
+    <ThemeProvider theme={themes} >
+      <div className='container'>
+        <UserSidebar />
+        <div className='main-content'>
+          <h2>User profile gose here</h2>
+          <p>main content goes here</p>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
